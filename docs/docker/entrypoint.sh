f2cc302d7e5bd8af1cf6a59e6217a0d9f1e35a8e
y627756022@163.com <yg627756022>
2020-12-10 11:34:17 +0800

    remove

--- docs/docker/entrypoint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-echo "The application will start in ${SPRINGBOOT_SLEEP}s..." && sleep ${SPRINGBOOT_SLEEP}
-exec java ${JAVA_OPTS} ${SW_AGENT_NAMESPACE} ${SW_AGENT_NAME} ${SW_AGENT_COLLECTOR_BACKEND_SERVICES} ${SW_AGENT_SAMPLE} -Djava.security.egd=file:/dev/./urandom -javaagent:"${HOME}/agent/skywalking-agent.jar" -jar "${HOME}/app.jar" "$@"
